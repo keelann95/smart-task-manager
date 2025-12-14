2aadff004ec6e583415c68dace837f86649c6f63
keelann95 <sephen.mwaniki@student.moringaschool.com>
2025-12-14 15:24:04 +0300

    edits the routes to stop returning json data and render it in ejs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
  const express = require('express');
  const connectDB = require('./models/db');
  const app = express();
+ 
+const Task = require("./models/Task");
 
  require('dotenv').config();
  const PORT = process.env.PORT || 3000
@@ -9,16 +11,37 @@ const taskRoutes = require('./routes/taskRoutes')
 
 
 
+const methodOverride = require("method-override");
+
+app.use(express.urlencoded({ extended: true })); // For HTML form submissions
+app.use(methodOverride("_method"));
+
+
+ app.set('view engine', 'ejs');
+app.set("views", "./views");
+
+app.use('/tasks', taskRoutes)
+app.use(express.static("public"));
+
+
  //middleware
 app.use(express.json());
 
 connectDB()
 
-app.use('/api', taskRoutes)
 
-app.get('/', (req, res) => {
-  res.send('Smart Task Manager API is running!');
+// Dashboard route
+app.get('/', async (req, res) => {
+  try {
+    const tasks = await Task.find().sort({ urgencyScore: -1 });
+    res.render('dashboard', { tasks });
+  } catch (err) {
+    console.error(err);
+    res.render('error', { error: 'Unable to load tasks' });
+  }
 });
+
+
  app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
  })
